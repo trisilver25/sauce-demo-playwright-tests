344da3031dc0e37e2c162d7a26b77f654ca4f7cc
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2026-08-08 11:36:09 -0400

    Added a new attribute and method to Products to select the filter on the Products page.
    Added a new test in products.spec.ts to check the Z-A Filter.

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -6,6 +6,38 @@ test("Verify default A-Z filter", async ({ page }) => {
 
   const ProductsPage = new Products(page);
 
+  // Get the 1st product card to compare later.
+  let prevProduct = await ProductsPage.getNthProductCard(0);
+
+  // Get the 1st product card name
+  let prevProductName = await ProductsPage.getProductName(prevProduct);
+
+  // Get the current count of Product Cards displayed on the page
+  const count = await ProductsPage.getProductCount();
+
+  // Loop through the count of product cards, and compare the previous card to the current card.
+  // Verifiying the previous card is less than the current name. As A is less than B.
+  for (let i = 0; i < count; i++) {
+    if (i + 1 != count) {
+      let currProductName = await ProductsPage.getProductName(
+        await ProductsPage.getNthProductCard(i + 1),
+      );
+      await expect(prevProductName <= currProductName).toBeTruthy();
+      prevProductName = currProductName;
+    } else {
+      break;
+    }
+  }
+});
+
+test("Verify Z-A filter", async ({ page }) => {
+  await page.goto("inventory.html");
+
+  const ProductsPage = new Products(page);
+
+  // Select the Z-A Filter
+  await ProductsPage.setDropDownFilter("za");
+
   let prevProduct = await ProductsPage.getNthProductCard(0);
 
   let prevProductName = await ProductsPage.getProductName(prevProduct);
@@ -17,7 +49,7 @@ test("Verify default A-Z filter", async ({ page }) => {
       let currProductName = await ProductsPage.getProductName(
         await ProductsPage.getNthProductCard(i + 1),
       );
-      await expect(prevProductName <= currProductName).toBeTruthy();
+      await expect(prevProductName >= currProductName).toBeTruthy();
       prevProductName = currProductName;
     } else {
       break;
